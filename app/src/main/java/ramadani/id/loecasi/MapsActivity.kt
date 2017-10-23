adc1dda91cf89a5eba395b03b95d951710dc510e
Ramadani <email.ramadani@gmail.com>
2017-10-23 14:30:52 +0700

    remove .idea

--- app/src/main/java/ramadani/id/loecasi/MapsActivity.kt
+++ app/src/main/java/ramadani/id/loecasi/MapsActivity.kt
@@ -1,65 +1,85 @@
 package ramadani.id.loecasi
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-
-import com.google.android.gms.maps.CameraUpdateFactory
+import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private var mMap: GoogleMap? = null
 
     companion object {
         val LOG_TAG = MapsActivity::class.java.simpleName
+        val ACCESS_MY_LOCATION_PERMISSIONS_REQUEST = 10001
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        if (requestCode == ACCESS_MY_LOCATION_PERMISSIONS_REQUEST) {
+            if (permissions.size == 1 && permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                mMap?.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(this, "Access location permission was denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(-7.79,110.366)
-        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(sydney, 16f)
-        val mapMarker = MarkerOptions().position(sydney).title("Yogyakarta")
-        mapMarker.draggable(true)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getPermissionToAccessFineLocation()
+        }
 
-        mMap!!.addMarker(mapMarker)
-        mMap!!.animateCamera(cameraUpdate)
-        mMap!!.setOnMarkerDragListener(this)
+        mMap?.uiSettings?.isMapToolbarEnabled = false
+        mMap?.setOnMyLocationButtonClickListener(this)
+        mMap?.setOnMyLocationClickListener(this)
     }
 
-    override fun onMarkerDragStart(marker: Marker?) {
-
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
+        return false
     }
 
-    override fun onMarkerDrag(marker: Marker?) {
-
+    override fun onMyLocationClick(location: Location) {
+        Log.d(LOG_TAG, "onMyLocationClick")
+        Toast.makeText(
+                this,
+                "Current location LAT ${location.latitude} LONG ${location.longitude}",
+                Toast.LENGTH_SHORT
+        ).show()
     }
 
-    override fun onMarkerDragEnd(marker: Marker?) {
-        Log.d(LOG_TAG, "Position ${marker?.position}")
+    @TargetApi(Build.VERSION_CODES.M)
+    private fun getPermissionToAccessFineLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mMap?.isMyLocationEnabled = true
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    ACCESS_MY_LOCATION_PERMISSIONS_REQUEST)
+        }
     }
 }
