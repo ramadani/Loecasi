ddae4d77886da0362d934812c2f31062f8ff2ff4
Ramadani <email.ramadani@gmail.com>
2017-10-30 17:25:09 +0700

    setup firestore & test add user

---
+++ app/src/main/java/org/loecasi/android/data/model/User.kt
@@ -0,0 +1,6 @@
+package org.loecasi.android.data.model
+
+/**
+ * Created by dani on 10/30/17.
+ */
+data class User(val name: String, val email: String)
--- app/src/main/java/org/loecasi/android/feature/signin/SignInPresenter.kt
+++ app/src/main/java/org/loecasi/android/feature/signin/SignInPresenter.kt
@@ -1,9 +1,16 @@
 package org.loecasi.android.feature.signin
 
+import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.loecasi.android.data.model.User
 import org.loecasi.android.feature.base.BasePresenter
+import java.util.concurrent.Callable
 import javax.inject.Inject
 
 /**
@@ -13,10 +20,12 @@ class SignInPresenter<V : SignInMvpView> @Inject constructor() : BasePresenter<V
         SignInMvpPresenter<V> {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onAttach(view: V) {
         super.onAttach(view)
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
     }
 
     override fun onCheckAuth() {
@@ -30,13 +39,30 @@ class SignInPresenter<V : SignInMvpView> @Inject constructor() : BasePresenter<V
     }
 
     override fun authWithGoogle(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                getView()?.openMainScreen()
-            } else {
-                getView()?.authFailed()
-            }
+        registerUser(User(account.displayName!!, account.email!!))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Log.d("SignIn", it)
+                }, {
+                    Log.e("SignIn", it.localizedMessage, it)
+                })
+
+//        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+//        auth.signInWithCredential(credential).addOnCompleteListener { task ->
+//            if (task.isSuccessful) {
+//                getView()?.openMainScreen()
+//            } else {
+//                getView()?.authFailed()
+//            }
+//        }
+    }
+
+    private fun registerUser(user: User): Observable<String> {
+        return Observable.create { subscribe ->
+            db.collection("users").add(user)
+                    .addOnSuccessListener { subscribe.onNext(it.id) }
+                    .addOnFailureListener { subscribe.onError(it) }
         }
     }
 }
